766591bf049e6289c2e2f626b66f82e5bce44308
Andrey Shi <andy.roshitu@gmail.com>
2023-03-31 00:27:18 +0300

    fix: eslint

--- project/src/components/Catalogs/CatalogGenresList/elements/GenreItem.tsx
+++ project/src/components/Catalogs/CatalogGenresList/elements/GenreItem.tsx
@@ -1,7 +1,6 @@
 import React, {SyntheticEvent} from 'react';
 import {GenreName} from '../../../../types/genre';
 import {useAppDispatch} from '../../../../hooks/useAppDispatch';
-import { useAppSelector } from '../../../../hooks/useAppSelector';
 import {useSelector} from 'react-redux';
 import {getGenreSelected} from '../../../../store/Genre/selectors/getGenreSelected/getGenreSelected';
 import {genreActions} from '../../../../store/Genre/slice/genreSlice';
